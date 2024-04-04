2edb8871ef7587e210963d2604807846bfcb5082
3DGamerexe <81819671+3DGamerexe@users.noreply.github.com>
2024-04-04 13:27:24 -0500

    Segue Kind of Working.

--- Ind03_madison_aaliyah/TableViewController.swift
+++ Ind03_madison_aaliyah/TableViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController {    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -23,12 +25,12 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
 
     /*

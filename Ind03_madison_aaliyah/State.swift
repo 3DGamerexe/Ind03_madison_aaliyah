c0f6985e252c817f77f1351391cf66e15b1adfb2
3DGamerexe <81819671+3DGamerexe@users.noreply.github.com>
2024-04-04 20:26:13 -0500

    Imported flags and maps up to Florida

--- Ind03_madison_aaliyah/State.swift
+++ Ind03_madison_aaliyah/State.swift
@@ -13,12 +13,12 @@ struct State {
     let name: String
     let map: UIImage
     let area: String
-    
 }
 
 let stateInfo: [State] = [
     State(flag: UIImage(named: "ALflag")!, name: "Alabama", map: UIImage(named: "Alabama")!, area: "50,744"),
-    State(flag: UIImage(named: "AKflag")!, name: "Alaska", map: UIImage(named: "AK")!, area: "663,267")
+    State(flag: UIImage(named: "AKflag")!, name: "Alaska", map: UIImage(named: "AK")!, area: "663,267"),
+    State(flag: UIImage(named: "AZflag")!, name: "Arizona", map: UIImage(named: "AZ")!, area: "113,998")
     
 ]
 
